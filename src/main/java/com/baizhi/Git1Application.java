5f72018f9dd89025dde1c458d579929ef3cf6809
tcs <tcszzu@126.com>
2019-08-27 15:22:54 +0800

    version1.0

--- src/main/java/com/baizhi/Git1Application.java
+++ src/main/java/com/baizhi/Git1Application.java
@@ -9,6 +9,7 @@ public class Git1Application {
     public static void main(String[] args) {
         SpringApplication.run(Git1Application.class, args);
         System.out.println("my name is tcs");
+        System.out.println("我想静静!");
     }
 
 }
