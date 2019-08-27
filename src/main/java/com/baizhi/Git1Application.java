bf1adcb10d697823c9be36587ddd69af79edddbf
tcs <tcszzu@126.com>
2019-08-27 14:23:31 +0800

    init

---
+++ src/main/java/com/baizhi/Git1Application.java
@@ -0,0 +1,14 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git1Application.class, args);
+        System.out.println("my name is tcs");
+    }
+
+}
